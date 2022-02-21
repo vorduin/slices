d806d44a95edc0b6d5f20223cb850ec667b66be6
Vorduin <vorduin@protonmail.com>
2022-02-21 16:45:39 +0200

    Switched to a BSD-style license

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // Copyright © The Slices Author. All rights reserved.
-// Use of this source logic, code, or documentation is governed by
-// an MIT-style license that can be found in the LICENSE file.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 // Package slices is a utility package for working with slices,
 // just like the strings and bytes packages. 
