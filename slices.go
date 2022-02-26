5d56e658ad6196868a8d733bfa8d60e437d825a0
Vorduin <vorduin@protonmail.com>
2022-02-27 01:24:01 +0200

    Wrote more functions and tests

--- slices.go
+++ slices.go
@@ -18,14 +18,192 @@ func WithCap[T any](c int) []T {
 
 // Clone makes a new slice and copies the given slice's elements into it.
 func Clone[T any](s []T) []T {
+	if s == nil {
+		return nil
+	}
+
 	cp := make([]T, len(s))
 	copy(cp, s)
 
 	return cp
 }
 
+// Reverse returns a copy of the slice s with order of the elements reversed.
+func Reverse[T any](s []T) []T {
+	cp := Clone(s)
+
+	for i, j := 0, len(cp)-1; i < j; i, j = i+1, j-1 {
+		cp[i], cp[j] = cp[j], cp[i]
+	}
+
+	return cp
+}
+
+// Last returns the last element in a slice.
+// Panics if s is empty.
+func Last[T any](s []T) T {
+	if len(s) == 0 {
+		panic("slices: empty slice")
+	}
+
+	return s[len(s)-1]
+}
+
+// Contains returns whether or not a slice contains a certain element.
+func Contains[T constraints.Ordered](s []T, el T) bool {
+	for _, x := range s {
+		if x == el {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Count counts the number of instances of el in s
+func Count[T constraints.Ordered](s []T, el T) int {
+	var c int
+	for _, x := range s {
+		if x == el {
+			c += 1
+		}
+	}
+
+	return c
+}
+
+// Index returns the index of the first instance of el in s,
+// or -1 if el is not present in s.
+func Index[T constraints.Ordered](s []T, el T) int {
+	for i, x := range s {
+		if x == el {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// Join joins two slices together.
+// If an argument is nil, it returns a clone
+// of the other argument.
+// If both are nil, it returns nil.
+func Join[T any](lhs, rhs []T) []T {
+	if lhs == nil && rhs == nil {
+		return nil
+	} else if lhs == nil {
+		return Clone(rhs)
+	} else if rhs == nil {
+		return Clone(lhs)
+	}
+
+	s := make([]T, len(lhs) + len(rhs))
+	copy(s, lhs)
+	copy(s[len(lhs):], rhs)
+
+	return s
+}
+
+// LastIndex returns the index of the last instance of el in s,
+// or -1 if el is not present in s. 
+func LastIndex[T constraints.Ordered](s []T, el T) int {
+	for i := len(s)-1; i >= 0; i-- {
+		if s[i] == el {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// Map returns a copy of the slice s with all its elements modified
+// according to the mapping function.
+func Map[T any](mapping func(T) T, s []T) []T {
+	cp := Clone(s)
+
+	for i := 0; i < len(s); i++ {
+		cp[i] = mapping(s[i])
+	}
+
+	return cp
+}
+
+// Repeat returns a new slice consisting of count copies of the slice s.
+// It panics if count is negative or if the result of (len(s) * count)
+// overflows.
+func Repeat[T any](s []T, count int) []T {
+	if s == nil {
+		return nil
+	}
+
+	if count == 0 {
+		return []T{}
+	}
+
+	if count < 0 {
+		panic("slices: negative Repeat count")
+	} else if len(s)*count/count != len(s) {
+		panic("slices: Repeat count causes overflow")
+	}
+
+	res := make([]T, len(s) * count)
+	for i := 0; i < count; i++ {
+		copy(res[i*len(s):(i+1)*len(s)], s)
+	}
+
+	return res
+}
+
+// Replace returns a copy of the slice s with the first n instances
+// of old replaced by new. If n < 0, there is no limit on
+// the number of replacements.
+func Replace[T constraints.Ordered](s []T, old, new T, n int) []T {
+	if s == nil {
+		return nil
+	}
+
+	cp := make([]T, len(s))
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == old && n != 0 {
+			cp[i] = new
+			n--
+		} else {
+			cp[i] = s[i]
+		}
+	}
+
+	return cp
+}
+
+// ReplaceAll returns a copy of the slice s with all instances of
+// old replaced by new.
+func ReplaceAll[T constraints.Ordered](s []T, old, new T) []T {
+	if s == nil {
+		return nil
+	}
+
+	cp := make([]T, len(s))
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == old {
+			cp[i] = new
+		} else {
+			cp[i] = s[i]
+		}
+	}
+
+	return cp
+}
+
 // Equal returns whether or not two slices are equal.
 func Equal[T constraints.Ordered](a, b []T) bool {
+	if a == nil && b == nil {
+		return true
+	} else if a == nil || b == nil {
+		return false
+	}
+
 	if len(a) != len(b) {
 		return false
 	}
@@ -50,6 +228,10 @@ func Sum[T constraints.Integer | constraints.Float](s []T) T {
 
 // Prod returns the product of the elements of the slice.
 func Prod[T constraints.Integer | constraints.Float](s []T) T {
+	if len(s) == 0 {
+		return 0
+	}
+
 	var prod T = 1
 	for i := 0; i < len(s); i++ {
 		prod *= s[i]
--- slices_test.go
+++ slices_test.go
@@ -5,15 +5,21 @@
 package slices_test
 
 import (
-	"github.com/vorduin/slices"
 	"testing"
+
+	"github.com/vorduin/slices"
 )
 
 func TestWithLen(t *testing.T) {
 	s := slices.WithLen[int](10)
 
 	if len(s) != 10 {
-		t.Failed()
+		t.Fail()
+	}
+
+	s = slices.WithLen[int](0)
+	if s == nil {
+		t.Fail()
 	}
 }
 
@@ -25,50 +31,312 @@ func TestWithCap(t *testing.T) {
 	}
 
 	if cap(s) != 10 {
-		t.Failed()
+		t.Fail()
+	}
+
+	s = slices.WithCap[int](0)
+	if s == nil {
+		t.Fail()
 	}
 }
 
-func TestCloneInt(t *testing.T) {
+func TestClone(t *testing.T) {
 	orig := []int{1, 2, 3}
 	cp := slices.Clone(orig)
 	orig[0] = 0
 
 	if cp[0] == 0 {
+		t.Fail()
+	}
+
+	orig = []int{}
+	cp = slices.Clone(orig)
+	if cp == nil {
+		t.Fail()
+	}
+
+	orig = nil
+	cp = slices.Clone(orig)
+	if cp != nil {
+		t.Fail()
+	}
+}
+
+func TestReverse(t *testing.T) {
+	s := []int{1, 2, 2, 3}
+	rev := slices.Reverse(s)
+
+	for i := 0; i < len(s); i++ {
+		if s[i] != rev[len(rev)-1-i] {
+			t.Fail()
+		}
+	}
+
+	s = nil
+	rev = slices.Reverse(s)
+	if rev != nil {
+		t.Fail()
+	}
+}
+
+func TestLast(t *testing.T) {
+	s := []int{1, 2, 3}
+
+	if slices.Last(s) != s[len(s)-1] {
 		t.Failed()
 	}
 }
 
-func TestCloneString(t *testing.T) {
-	orig := []string{"the", "slices", "author"}
-	cp := slices.Clone(orig)
-	orig[0] = "a"
+func TestContains(t *testing.T) {
+	s := []int{1, 2, 3}
 
-	if cp[0] == "a" {
-		t.Failed()
+	if !slices.Contains(s, 2) {
+		t.Fail()
+	}
+
+	if slices.Contains(s, 4) {
+		t.Fail()
+	}
+
+	s = nil
+	if slices.Contains(s, 1) {
+		t.Fail()
 	}
 }
 
-func TestClone2DSlice(t *testing.T) {
-	orig := [][]int{{1}, {2}, {3}}
-	cp := slices.Clone(orig)
-	orig[0] = []int{0}
+func TestCount(t *testing.T) {
+	s := []int{1, 2, 2, 3}
 
-	if cp[0][0] == 0 {
-		t.Failed()
+	if slices.Count(s, 0) != 0 {
+		t.Fail()
+	}
+
+	if slices.Count(s, 1) != 1 {
+		t.Fail()
+	}
+
+	if slices.Count(s, 2) != 2 {
+		t.Fail()
+	}
+
+	s = nil
+	if slices.Count(s, 1) != 0 {
+		t.Fail()
 	}
 }
 
-func TestEqualDiffLen(t *testing.T) {
-	s1 := []int{1, 2, 3}
-	s2 := []int{1, 2, 3, 4, 5}
+func TestIndex(t *testing.T) {
+	s := []int{1, 2, 2, 3}
 
-	if slices.Equal(s1, s2) {
-		t.Failed()
+	if slices.Index(s, 2) != 1 {
+		t.Fail()
+	}
+
+	if slices.Index(s, 0) != -1 {
+		t.Fail()
+	}
+
+	s = nil
+	if slices.Index(s, 1) != -1 {
+		t.Fail()
 	}
 }
 
-func TestEqualInt(t *testing.T) {
+func TestJoin(t *testing.T) {
+	lhs := []int{1, 2, 3}
+	rhs := []int{4, 5, 6}
+	j := slices.Join(lhs, rhs)
+	if !slices.Equal(j, []int{1, 2, 3, 4, 5, 6}) {
+		t.Fail()
+	}
+
+	lhs = []int{}
+	j = slices.Join(lhs, rhs)
+	if !slices.Equal(j, []int{4, 5, 6}) {
+		t.Fail()
+	}
+
+	lhs = nil
+	j = slices.Join(lhs, rhs)
+	if !slices.Equal(j, []int{4, 5, 6}) {
+		t.Fail()
+	}
+
+	lhs = []int{1, 2, 3}
+	rhs = []int{}
+	j = slices.Join(lhs, rhs)
+	if !slices.Equal(j, []int{1, 2, 3}) {
+		t.Fail()
+	}
+
+	rhs = nil
+	j = slices.Join(lhs, rhs)
+	if !slices.Equal(j, []int{1, 2, 3}) {
+		t.Fail()
+	}
+
+	lhs = []int{}
+	rhs = []int{}
+	j = slices.Join(lhs, rhs)
+	if j == nil || len(j) != 0 {
+		t.Fail()
+	}
+
+	lhs = nil
+	rhs = nil
+	j = slices.Join(lhs, rhs)
+	if j != nil {
+		t.Fail()
+	}
+}
+
+func TestLastIndex(t *testing.T) {
+	s := []int{1, 2, 2, 3}
+
+	if slices.LastIndex(s, 2) != 2 {
+		t.Fail()
+	}
+
+	if slices.LastIndex(s, 0) != -1 {
+		t.Fail()
+	}
+
+	s = nil
+	if slices.LastIndex(s, 1) != -1 {
+		t.Fail()
+	}
+}
+
+func TestMap(t *testing.T) {
+	s := []int{0, 1, 2}
+	f := func(i int) int {
+		return i + 1
+	}
+
+	mapped := slices.Map(f, s)
+
+	if !slices.Equal(mapped, []int{1, 2, 3}) {
+		t.Fail()
+	}
+
+	s = nil
+	mapped = slices.Map(f, s)
+	if mapped != nil {
+		t.Fail()
+	}
+}
+
+func TestRepeat(t *testing.T) {
+	s := []int{1, 2, 3}
+	r := slices.Repeat(s, 2)
+
+	if !slices.Equal(r, []int{1, 2, 3, 1, 2, 3}) {
+		t.Fail()
+	}
+
+	r = slices.Repeat(s, 0)
+	if r == nil || len(r) != 0 {
+		t.Fail()
+	}
+
+	s = []int{}
+	r = slices.Repeat(s, 2)
+
+	if r == nil || len(s) != 0 {
+		t.Fail()
+	}
+
+	s = nil
+	r = slices.Repeat(s, 2)
+
+	if r != nil {
+		t.Fail()
+	}
+}
+
+func TestReplace(t *testing.T) {
+	s := []int{1, 2, 2, 3}
+	r := slices.Replace(s, 2, 0, 1)
+	if !slices.Equal(r, []int{1, 0, 2, 3}) {
+		t.Fail()
+	}
+
+	s = []int{1, 2, 2, 3}
+	r = slices.Replace(s, 2, 0, 2)
+	if !slices.Equal(r, []int{1, 0, 0, 3}) {
+		t.Fail()
+	}
+
+	s = []int{1, 2, 2, 3}
+	r = slices.Replace(s, 2, 0, -1)
+	if !slices.Equal(r, []int{1, 0, 0, 3}) {
+		t.Fail()
+	}
+
+	s = []int{1, 2, 2, 3}
+	r = slices.Replace(s, 0, 5, 1)
+	if !slices.Equal(r, []int{1, 2, 2, 3}) {
+		t.Fail()
+	}
+
+	s = []int{1, 2, 2, 3}
+	r = slices.Replace(s, 0, 5, -1)
+	if !slices.Equal(r, []int{1, 2, 2, 3}) {
+		t.Fail()
+	}
+
+	s = []int{}
+	r = slices.Replace(s, 0, 5, 1)
+	if r == nil || len(r) != 0 {
+		t.Fail()
+	}
+
+	s = []int{}
+	r = slices.Replace(s, 0, 5, -1)
+	if r == nil || len(r) != 0 {
+		t.Fail()
+	}
+
+	s = nil
+	r = slices.Replace(s, 0, 5, 1)
+	if r != nil {
+		t.Fail()
+	}
+
+	s = nil
+	r = slices.Replace(s, 0, 5, -1)
+	if r != nil {
+		t.Fail()
+	}
+}
+
+func TestReplaceAll(t *testing.T) {
+	s := []int{1, 2, 2, 3}
+	r := slices.ReplaceAll(s, 2, 0)
+	if !slices.Equal(r, []int{1, 0, 0, 3}) {
+		t.Fail()
+	}
+
+	s = []int{1, 2, 2, 3}
+	r = slices.ReplaceAll(s, 0, 5)
+	if !slices.Equal(r, []int{1, 2, 2, 3}) {
+		t.Fail()
+	}
+
+	s = []int{}
+	r = slices.ReplaceAll(s, 0, 5)
+	if r == nil || len(r) != 0 {
+		t.Fail()
+	}
+
+	s = nil
+	r = slices.ReplaceAll(s, 0, 5)
+	if r != nil {
+		t.Fail()
+	}
+}
+
+func TestEqual(t *testing.T) {
 	s1 := []int{1, 2, 3}
 	if !slices.Equal(s1, s1) {
 		t.Fail()
@@ -78,16 +346,62 @@ func TestEqualInt(t *testing.T) {
 	if slices.Equal(s1, s2) {
 		t.Fail()
 	}
-}
 
-func TestEqualString(t *testing.T) {
-	s1 := []string{"the", "slices", "author"}
-	if !slices.Equal(s1, s1) {
+	// different len
+	s1 = []int{1, 2, 3}
+	s2 = []int{1, 2, 3, 4, 5}
+
+	if slices.Equal(s1, s2) {
 		t.Fail()
 	}
 
-	s2 := []string{"a", "slices", "author"}
+	s1 = nil
+	s2 = []int{1, 2, 3}
 	if slices.Equal(s1, s2) {
+		t.Fail()
+	}
+
+	s1 = []int{1, 2, 3}
+	s2 = nil
+	if slices.Equal(s1, s2) {
+		t.Fail()
+	}
+
+	s1 = nil
+	s2 = nil
+	if !slices.Equal(s1, s2) {
+		t.Fail()
+	}
+}
+
+func TestSum(t *testing.T) {
+	s := []int{1, 2, 3}
+	sum := slices.Sum(s)
+
+	if sum != 1+2+3 {
+		t.Fail()
+	}
+
+	s = nil
+	sum = slices.Sum(s)
+
+	if sum != 0 {
+		t.Fail()
+	}
+}
+
+func TestProd(t *testing.T) {
+	s := []int{1, 2, 3}
+	prod := slices.Prod(s)
+
+	if prod != 1*2*3 {
+		t.Fail()
+	}
+
+	s = nil
+	prod = slices.Prod(s)
+
+	if prod != 0 {
 		t.Fail()
 	}
 }
